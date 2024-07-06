10f50a9840ae9d29619516cdd5c9e897304f6382
anilkumarsde <anilkumarlks16@gmail.com>
2024-07-06 14:33:23 +0530

    updated

--- 1.PATTERN/hello.cpp
+++ 1.PATTERN/hello.cpp
@@ -148,13 +148,44 @@ void lower(int n)
         cout << endl;
     }
 }
+void print10(int n)
+{
+    for (int i = 1; i <= 2 * n - 1; i++)
+    {
+        int star = i;
+        if (i > n)
+            star = 2 * n - i;
+        for (int j = 1; j <= star; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void print11(int n)
+{
+    int c = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+            c = 0;
+        else
+            c = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            cout << c << " ";
+            c = 1 - c;
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
     int n;
     cin >> n;
-    // print8(n);
-    upper(n);
-    lower(n);
+    print11(n);
+    // upper(n);
+    // lower(n);
     return 0;
 }
