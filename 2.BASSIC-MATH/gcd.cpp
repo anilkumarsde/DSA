b564b9df0e79455a030295b96f2c024473205cf8
anilkumarsde <anilkumarlks16@gmail.com>
2024-08-25 14:37:53 +0530

    updated

---
+++ 2.BASSIC-MATH/gcd.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int gcd(int a, int b)
+{
+    while (a > 0 && b > 0)
+    {
+        if (a > b)
+        {
+            a = a % b;
+        }
+        else
+        {
+            b = b % a;
+        }
+    }
+    if (a == 0)
+        return b;
+    else
+        return a;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a;
+    cin >> b;
+    cout << gcd(a, b);
+    return 0;
+}
