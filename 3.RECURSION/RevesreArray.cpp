21b095cd67533eb1f32947057e955b713e5accc6
anilkumarsde <anilkumarlks16@gmail.com>
2024-09-25 09:22:57 +0530

    updated

--- 3.RECURSION/RevesreArray.cpp
+++ 3.RECURSION/RevesreArray.cpp
@@ -1,26 +1,27 @@
-#include<iostream>
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<math.h>
 using namespace std;
-void fun(int i,int arr[],int n)
-{
-    if(i>n/2)
-    {
-        return;
-    }
+void pri(int i,int arr[],int n){
+    if(i>=n/2) return;
     swap(arr[i],arr[n-i-1]);
-    fun(i+1,arr,n);
+    i++;
+    pri(i,arr,n);
 }
-int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++)
-    {
-        cin>>arr[i];
-    }
-    fun(0,arr,n);
-      for(int i=0;i<n;i++)
-    {
-        cout<<arr[i]<<" ";
-    }
+
+int main() {
+    // Write C++ code here
+ int n;
+ cin>>n;
+ int arr[n];
+ for(int i=0;i<n;i++){
+     cin>>arr[i];
+ }
+ pri(0,arr,n);
+ for(int i=0;i<n;i++)
+ {
+     cout<<arr[i]<<endl;
+ }
+
     return 0;
 }
