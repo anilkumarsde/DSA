e05773aa5f652deb53885ed2057c7ab2323fdddc
anilkumarsde <anilkumarlks16@gmail.com>
2024-11-09 11:10:53 +0530

    updated

--- 5.ARRAY/2.BASIC QUESTION/largest.cpp
+++ 5.ARRAY/2.BASIC QUESTION/largest.cpp
@@ -1,17 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int FindLargest(int arr[],int n){
-    int lgt=arr[0];
-    for(int i=1;i<n;i++){
-        if(arr[i]>lgt){
-            lgt=arr[i];
+int FindLargest(int arr[], int n)
+{
+    int lgt = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > lgt)
+        {
+            lgt = arr[i];
         }
     }
     return lgt;
 }
-int main(){
-    int arr[]={4,5,24,7,8};
-    cout<<FindLargest(arr,5);
+int main()
+{
+    int arr[] = {4, 5, 2, 7, 8};
+    cout << FindLargest(arr, 5);
 
     return 0;
 }
